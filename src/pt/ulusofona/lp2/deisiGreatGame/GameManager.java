a7bebb987748c9409950d16bdaa627bf03dba2ff
mikecarreta <belgica95@outlook.com>
2021-11-24 23:12:23 +0000

    reactToAbyssOrTool fix return null

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -994,7 +994,7 @@ public class GameManager {
         } else if (isAbyss) {
             return "Ohhh, caiu num abismo !!";
         } else {
-            return null;
+            return "";
         }
 
 
