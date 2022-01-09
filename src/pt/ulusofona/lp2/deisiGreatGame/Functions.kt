6a833bd575c1ec4c042e314d28747715ff664f17
DrAlexWild <ricardogoncalves126@gmail.com>
2022-01-09 21:08:55 +0000

    mostUsedAbysses fix (1)

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -75,7 +75,7 @@ fun mostUsedAbysses(manager: GameManager, max_results : Int): String {
     var stringFinal : String = ""
 
     var res = manager.steppedOn
-        .entries.sortedBy { it.value }
+        .entries.filter { it.value > 0 }.sortedBy { it.value }
         .reversed().take(max_results).associate { it.toPair() }
 
     res.forEach { stringFinal += it.key + ":" + it.value + "\n" }
