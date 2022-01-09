a751ad5c2154a1b77dfb327bb082c0f7175b38d7
DrAlexWild <ricardogoncalves126@gmail.com>
2022-01-09 21:40:38 +0000

    mostUsedAbysses fix (3)

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -1268,9 +1268,9 @@ public class GameManager {
         casas.put("Duplicated Code", steppedOnDuplicatedCode);
         casas.put("Efeitos secundários", steppedOnSideEffect);
         casas.put("Blue Screen of Death", steppedOnBlueScreenDeath);
-        casas.put("File Not Found Exception", steppedOnFileNotFoundException);
         casas.put("Ciclo infinito", steppedOnInfiniteLoop);
         casas.put("Segmentation Fault", steppedOnSegmentationFault);
+        casas.put("File Not Found Exception", steppedOnFileNotFoundException);
 
         return casas;
     }
